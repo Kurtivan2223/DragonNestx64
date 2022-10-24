ac425e7cd100e98753bb8e18dac10120d560f4d6
Kurtivan2223 <kurtgaller8@gmail.com>
2022-10-24 21:42:09 +0800

    Adding full Ownership SQL Script

--- SQL/Query/Ownership.sql
+++ SQL/Query/Ownership.sql
@@ -8,14 +8,40 @@ GO
     BEGIN 
         EXEC sp_addlogin 'DragonNest', 'E6h7HsRXJbH8ays'  
     END
-GO
 
---Creating SQL User
-USE [master]
-GO
+    IF NOT EXISTS (SELECT * FROM [sysusers] WHERE name = 'DragonNest')
+    BEGIN
+        EXEC sp_adduser 'DragonNest', 'DragonNest'
+    END
+
     IF NOT EXISTS (SELECT * FROM [master].[dbo].[syslogins] WHERE name = 'SPExecutor') 
     BEGIN 
-        EXEC sp_addlogin 'SPExecutor'
+        EXEC sp_addlogin 'SPExecutor', 'E6h7HsRXJbH8ays'
+    END
+
+    IF NOT EXISTS (SELECT * FROM [sysusers] WHERE name = 'SPExecutor')
+    BEGIN
+        EXEC sp_adduser 'SPExecutor', 'SPExecutor'
+    END
+
+    IF NOT EXISTS (SELECT * FROM [master].[dbo].[syslogins] WHERE name = 'GSMAdmin') 
+    BEGIN 
+        EXEC sp_addlogin 'GSMAdmin', 'E6h7HsRXJbH8ays'
+    END
+
+    IF NOT EXISTS (SELECT * FROM [sysusers] WHERE name = 'GSMAdmin')
+    BEGIN
+        EXEC sp_adduser 'GSMAdmin', 'GSMAdmin'
+    END
+
+    IF NOT EXISTS (SELECT * FROM [master].[dbo].[syslogins] WHERE name = 'program_server') 
+    BEGIN 
+        EXEC sp_addlogin 'program_server', 'E6h7HsRXJbH8ays'
+    END
+
+    IF NOT EXISTS (SELECT * FROM [sysusers] WHERE name = 'program_server')
+    BEGIN
+        EXEC sp_adduser 'program_server', 'program_server'
     END
 GO
 
@@ -40,48 +66,74 @@ GO
 --creating SPExecutor user
 USE [DNMembership]
 GO
-CREATE USER [SPExecutor] FOR LOGIN [SPExecutor] WITH DEFAULT_SCHEMA=[SPExecutor]
+CREATE USER [SPExecutor] FOR LOGIN [SPExecutor] WITH DEFAULT_SCHEMA=[dbo]
 GO
 
 USE [DNServerLog]
 GO
-CREATE USER [SPExecutor] FOR LOGIN [SPExecutor] WITH DEFAULT_SCHEMA=[SPExecutor]
+CREATE USER [SPExecutor] FOR LOGIN [SPExecutor] WITH DEFAULT_SCHEMA=[dbo]
 GO
 
 USE [DNWorld]
 GO
-CREATE USER [SPExecutor] FOR LOGIN [SPExecutor] WITH DEFAULT_SCHEMA=[SPExecutor]
+CREATE USER [SPExecutor] FOR LOGIN [SPExecutor] WITH DEFAULT_SCHEMA=[dbo]
+GO
+
+--creating GSMAdmin user
+USE [DNMembership]
+GO
+CREATE USER [GSMAdmin] FOR LOGIN [GSMAdmin] WITH DEFAULT_SCHEMA=[dbo]
+GO
+
+USE [DNServerLog]
+GO
+CREATE USER [GSMAdmin] FOR LOGIN [GSMAdmin] WITH DEFAULT_SCHEMA=[dbo]
+GO
+
+USE [DNWorld]
+GO
+CREATE USER [GSMAdmin] FOR LOGIN [GSMAdmin] WITH DEFAULT_SCHEMA=[dbo]
+GO
+
+--creating program_server user
+USE [DNMembership]
+GO
+CREATE USER [program_server] FOR LOGIN [program_server] WITH DEFAULT_SCHEMA=[dbo]
+GO
+
+USE [DNServerLog]
+GO
+CREATE USER [program_server] FOR LOGIN [program_server] WITH DEFAULT_SCHEMA=[dbo]
+GO
+
+USE [DNWorld]
+GO
+CREATE USER [program_server] FOR LOGIN [program_server] WITH DEFAULT_SCHEMA=[dbo]
 GO
 
 --Ownership role
 USE [DNMembership]
 GO
 EXEC [dbo].[sp_addrolemember] 'db_owner', 'DragonNest'
+EXEC [dbo].[sp_addrolemember] 'db_owner', 'SPExecutor'
+EXEC [dbo].[sp_addrolemember] 'db_owner', 'GSMAdmin'
+EXEC [dbo].[sp_addrolemember] 'db_owner', 'program_server'
 GO
 
 USE [DNServerLog]
 GO
 EXEC [dbo].[sp_addrolemember] 'db_owner', 'DragonNest'
+EXEC [dbo].[sp_addrolemember] 'db_owner', 'SPExecutor'
+EXEC [dbo].[sp_addrolemember] 'db_owner', 'GSMAdmin'
+EXEC [dbo].[sp_addrolemember] 'db_owner', 'program_server'
 GO
 
 USE [DNWorld]
 GO
 EXEC [dbo].[sp_addrolemember] 'db_owner', 'DragonNest'
-GO
-
-USE [DNMembership]
-GO
 EXEC [dbo].[sp_addrolemember] 'db_owner', 'SPExecutor'
-GO
-
-USE [DNServerLog]
-GO
-EXEC [dbo].[sp_addrolemember] 'db_owner', 'SPExecutor'
-GO
-
-USE [DNWorld]
-GO
-EXEC [dbo].[sp_addrolemember] 'db_owner', 'SPExecutor'
+EXEC [dbo].[sp_addrolemember] 'db_owner', 'GSMAdmin'
+EXEC [dbo].[sp_addrolemember] 'db_owner', 'program_server'
 GO
 
 --If Database already has ownership from previous SQL Server
@@ -89,29 +141,36 @@ GO
 USE [DNMembership]
 GO
 EXEC [dbo].[sp_change_users_login] 'Update_One', 'DragonNest', 'DragonNest'
+EXEC [dbo].[sp_change_users_login] 'Update_One', 'SPExecutor', 'SPExecutor'
+EXEC [dbo].[sp_change_users_login] 'Update_One', 'GSMAdmin', 'GSMAdmin'
+EXEC [dbo].[sp_change_users_login] 'Update_One', 'program_server', 'program_server'
 GO
 
 USE [DNServerLog]
 GO
 EXEC [dbo].[sp_change_users_login] 'Update_One', 'DragonNest', 'DragonNest'
+EXEC [dbo].[sp_change_users_login] 'Update_One', 'SPExecutor', 'SPExecutor'
+EXEC [dbo].[sp_change_users_login] 'Update_One', 'GSMAdmin', 'GSMAdmin'
+EXEC [dbo].[sp_change_users_login] 'Update_One', 'program_server', 'program_server'
 GO
 
 USE [DNWorld]
 GO
 EXEC [dbo].[sp_change_users_login] 'Update_One', 'DragonNest', 'DragonNest'
+EXEC [dbo].[sp_change_users_login] 'Update_One', 'SPExecutor', 'SPExecutor'
+EXEC [dbo].[sp_change_users_login] 'Update_One', 'GSMAdmin', 'GSMAdmin'
+EXEC [dbo].[sp_change_users_login] 'Update_One', 'program_server', 'program_server'
 GO
 
-USE [DNMembership]
-GO
-EXEC [dbo].[sp_change_users_login] 'Update_One', 'SPExecutor', 'SPExecutor'
-GO
-
-USE [DNServerLog]
-GO
-EXEC [dbo].[sp_change_users_login] 'Update_One', 'SPExecutor', 'SPExecutor'
+--Resetting Password
+sp_password NULL, 'E6h7HsRXJbH8ays', 'DragonNest';
 GO
 
-USE [DNWorld]
+sp_password NULL, 'E6h7HsRXJbH8ays', 'SPExecutor';
 GO
-EXEC [dbo].[sp_change_users_login] 'Update_One', 'SPExecutor', 'SPExecutor'
+
+sp_password NULL, 'E6h7HsRXJbH8ays', 'GSMAdmin';
+GO
+
+sp_password NULL, 'E6h7HsRXJbH8ays', 'program_server';
 GO
